d5cebb8e7fdad2d217add73c34d0a9e2551981ef
gvsn13 <86527158+gvsn13@users.noreply.github.com>
2024-03-22 00:36:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Timestamp-Converter
+You can check it out here: https://time-converter-gvsn.netlify.app/# time-converter
---
+++ epochtohuman.js
@@ -0,0 +1,44 @@
+const year=document.querySelector('#year-output');
+const month=document.querySelector('#month-output');
+const date=document.querySelector('#date-output');
+const hour=document.querySelector('#hour-output');
+const minute=document.querySelector('#minute-output');
+const second=document.querySelector('#second-output');
+const button=document.querySelector('#convert-button');
+const input=document.querySelector('#epoch-input');
+const resetButton=document.querySelector('#reset-button');
+const currentEpoch=document.querySelector('#current-epoch');
+const localTime=document.querySelector('#local-time');
+
+
+setInterval(()=>{
+  currentEpoch.innerHTML = Math.floor(new Date().getTime()/1000);
+  //currentEpoch.innerHTML = Math.floor(Date.now()/1000);   //this works too..
+},1000);
+
+setInterval(()=>{
+  localTime.innerHTML = new Date();
+},1000);
+
+resetButton.addEventListener('click',()=>{
+  input.value = "";
+  year.value = "";
+  month.value = "";
+  date.value = "";
+  hour.value = "";
+  minute.value = "";
+  second.value = "";
+});
+
+
+
+button.addEventListener('click',()=>{
+  let epochDate = new Date(parseInt(input.value)*1000);//1611041456
+  year.value = epochDate.getFullYear();
+  month.value = epochDate.getMonth()+1;
+  date.value = epochDate.getDate();
+  hour.value = epochDate.getHours();
+  minute.value = epochDate.getMinutes();
+  second.value = epochDate.getSeconds(); 
+});
+
